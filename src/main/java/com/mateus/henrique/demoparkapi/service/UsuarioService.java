956112c00f9dadd7de50ddef307cf5284241244b
Mateus Henrique <mateushbsb@gmail.com>
2023-11-28 16:28:41 -0300

    Feature: add user

--- src/main/java/com/mateus/henrique/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/mateus/henrique/demoparkapi/service/UsuarioService.java
@@ -2,13 +2,20 @@ package com.mateus.henrique.demoparkapi.service;
 
 import org.springframework.stereotype.Service;
 
+import com.mateus.henrique.demoparkapi.entity.Usuario;
 import com.mateus.henrique.demoparkapi.repository.UsuarioRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
   private final UsuarioRepository usuarioRepository;
+
+  @Transactional
+  public Usuario salvar(Usuario usuario) {
+    return usuarioRepository.save(usuario);
+  }
 
 }
